8313a5e5afa20b2536fcd392167112d9c90a9bcf
brentritzema <brentritztro96@gmail.com>
2018-02-21 09:11:45 -0500

    Skeleton of shell

--- JBShell.cpp
+++ JBShell.cpp
@@ -1,11 +1,15 @@
 /*
- * Main file for jbshell (which is just a shell)
+ * Shell file for jbshell (which is just a shell)
  * Created by: Brent Ritzema and Jesse Kuntz for CS232
  * Calvin College Spring 2018
  */
 
 #include "JBShell.h"
 
-int main() {
-    return 0;
+JBShell::JBShell() {
+
+}
+
+void JBShell::run() {
+
 }
---
+++ JBShell.h
@@ -0,0 +1,17 @@
+/*
+ * Shell file for jbshell (which is just a shell)
+ * Created by: Brent Ritzema and Jesse Kuntz for CS232
+ * Calvin College Spring 2018
+ */
+
+#ifndef JBSHELL_H
+#define JBSHELL_H
+
+class JBShell {
+public:
+    JBShell();
+    void run();
+private:
+};
+
+#endif
