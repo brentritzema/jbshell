5d07c2a04cb7b2375cbec64f64aeb101611d060e
Jesse Kuntz <jessekuntz96@gmail.com>
2018-02-22 11:23:04 -0500

    - Full implementation (w/ incomplete error handling)

--- Path.cpp
+++ Path.cpp
@@ -1,7 +1,18 @@
 #include "Path.h"
 
 /******************************************
- * Necessary methods, as defined by the 
+ * To figure out as a pair:
+ * - Do I need to send any error handling?
+ * Or should I be printing it?
+ * - opendir() error handling
+ * - getDirectory() error handling
+ * - Path() error handling
+ * - Right now it avoids all errors, just doesn't
+ * handle them.
+ * ****************************************/
+
+/******************************************
+ * Necessary methods, as defined by the
  * assignment.
  * ****************************************/
 
@@ -11,33 +22,65 @@ Path::Path() {
         string sPath(pPath);
         populateVPath(sPath);
 
-        printVPath(vPath);
+        // FOR TESTING
+        //  printVPath(vPath);
     }
+    // Do something if the PATH IS Null?
 }
 
 int Path::find(const string& program) const {
     int index = -1;
     for (int i = 0; i < vPath.size(); i++) {
-        if (vPath[i].find(program) != string::npos) index = i;
+        // The following is based off of the code found at:
+        // http://www.qnx.com/developers/docs/6.5.0/index.jsp?topic=%2Fcom.qnx.doc.neutrino_lib_ref%2Fo%2Fopendir.html
+        DIR* dirp;
+        struct dirent* direntp;
+
+        dirp = opendir(vPath[i].c_str());
+        if (dirp != NULL) {
+            for(;;) {
+                direntp = readdir(dirp);
+                if(direntp == NULL) break;
+                if(direntp->d_name == program) {
+                    index = i;
+                    break;
+                }
+                // FOR TESTING
+                // cout << direntp->d_name << endl;
+            }
+            closedir(dirp);
+        } else {
+            // FOR TESTING
+            // I mean... don't really need to print this.
+            // perror(vPath[i].c_str());
+        }
     }
-    cout << index;
+    // FOR TESTING
+    // cout << index << endl;
+    // cout << vPath[index] << endl;
     return index;
 }
 
 string Path::getDirectory(int i) const {
-    return vPath[i];
+    if (i > vPath.size() || i < 0) {
+        cout << "Index out of bounds." << endl;
+        // What do I return here?
+        return "-1";
+    } else {
+        return vPath[i];
+    }
 }
 
 
 /******************************************
- * Methods to help keep things clean 
+ * Methods to help keep things clean
  * and readable.
  * ****************************************/
 
 // Helper method for debugging
 void Path::printVPath(vector<string> v) {
-    for (int x = 0; x != v.size(); ++x) {
-        cout << v[x] << endl;
+    for (int i = 0; i != v.size(); ++i) {
+        cout << i << ". " << v[i] << endl;
     }
 }
 
@@ -55,4 +98,6 @@ void Path::populateVPath(string s) {
     token = s.substr(i, s.find(":", i) - i);
     vPath.push_back(token);
 }
+
+
 
--- Path.h
+++ Path.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <dirent.h>
 
 using namespace std;
 
